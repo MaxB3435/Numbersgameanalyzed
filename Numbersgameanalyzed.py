c80c90cb2995f4b3be3331c921e30b70512b2232
MaxB3435 <bortasmax@gmail.com>
2024-02-20 09:12:50 +0100

    Update Numbersgameanalyzed.py

--- Numbersgameanalyzed.py
+++ Numbersgameanalyzed.py
@@ -1,28 +1,32 @@
 import random
 ng = 1
-
 h = 100
 l = 1
 g = int(h/2)
-
-#print ('Välj ett nummer')
-#number = int(input())
+game = 1
+ngtot = 0
 number = random.randint(l,h)
-print(number)
-while True:
-    if g != number:
-        ng = ng +1
-        print(g)
-        if g > number:
-            print('Nix, lägre')
-            h = g - 1
-            g = int((h+l)/2)
-        elif g < number:
-            l = g + 1
-            print('Nix, högre')
-            g = int((h+l)/2)
-    else:
-         print('Jag klarade spelet på', ng,'gissningar!')
-         exit()
 
-    
+
+while True:
+    if game < 10000:
+        if g != number:
+            ng = ng +1
+            if g > number:
+               h = g - 1
+               g = int((h+l)/2)
+            elif g < number:
+                l = g + 1
+                g = int((h+l)/2)
+        else:
+            game = game +1
+            h = 100
+            l = 1
+            g = int(h/2)
+            number = random.randint(l,h)
+            ngtot = ngtot + ng
+            ng = 1
+    else:
+        avg = ngtot / 10000
+        print(avg)
+        exit()
